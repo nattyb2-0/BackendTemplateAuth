55aa8931dd6322e8011d5f534a92f32f79717c88
Natty Davis <natty.davis79@gmail.com>
2017-11-19 16:07:58 -0500

    set up simple server and tested to see if up and running

---
+++ server.js
@@ -0,0 +1,28 @@
+// require our dependencies for the application
+const express = require('express'),
+      logger = require('morgan'),
+      bodyParser = require('body-parser'),
+      path = require('path');
+
+
+//instantiate app
+const app = express();
+
+
+//use or middleware modules
+//for logging outputs to dev server
+app.use(logger('dev'));
+//for parsing json objects
+app.use(bodyParser.json())
+//for parsing urls
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//set port to env variable for production or 3000 for development
+const Port = process.env.PORT || 3005
+
+
+
+
+app.listen(Port, (req,res)=>{
+  console.log(`app is very much in good health and running on port ${Port}`)
+})
